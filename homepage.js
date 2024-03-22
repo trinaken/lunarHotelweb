771763f4986de0d08d4eb8826e7f269c775805ad
trinaken <152493532+trinaken@users.noreply.github.com>
2024-03-22 09:00:23 +0000

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,14 @@
+function detectLoaded() 
+{
+alert("Page is Loaded");
+}
+
+function detectClick() 
+{
+alert("You Clicked a Link");
+}
+
+function detectDoubleClick() 
+{
+alert("You Double-Clicked another Link");
+}
